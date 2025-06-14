f2b92d883acf09cf8b6f3d66cee37894ac0a0707
PROTOTYPEJESUS <jesusandrade427@gmail.com>
2025-06-14 10:45:10 -0500

    Fix botones para exportar tablas del dashboard

--- src/admin/components/Content.jsx
+++ src/admin/components/Content.jsx
@@ -2,10 +2,14 @@ import Navbar from '../../frontend/components/Navbar';
 import Footer from "../../frontend/components/Footer"
 
 export default function Content() {
-  const descargarCSV = () => {
-  window.open("https://impulces-backend-724298271244.us-central1.run.app/emprendimientos/export/csv", "_blank");
+  const descargarCSVU = () => {
   window.open("https://impulces-backend-724298271244.us-central1.run.app/usuarios/export/csv", "_blank");
 };
+
+  const descargarCSVI = () =>{
+  window.open("https://impulces-backend-724298271244.us-central1.run.app/emprendimientos/export/csv", "_blank");
+  }
+  
   return (
     <>
           {/*<!-- Content Wrapper --> */}
@@ -14,12 +18,12 @@ export default function Content() {
             {/* <!-- Main Content --> */}
             <div id="content">
                 
-                    <div className="d-sm-flex align-items-center justify-content-end mb-4"> 
-                        <a href="C:\Users\nikol\Desktop\Impulces_V3\ImpulcesApp\src\frontend\pages\NotFoundPage.jsx" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                className="fas fa-download fa-sm text-white-50"></i> Generate Report
-                        </a>
-                        <button onClick={descargarCSV}className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-                        <i className="fas fa-download fa-sm text-white-50"></i> Generate CSV
+                    <div className="d-sm-flex align-items-center justify-content-center mb-4 gap-3"> 
+                        <button onClick={descargarCSVI}className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"> 
+                        <i className="fas fa-download fa-sm text-white-50"></i> Generate CSV Emprendimientos
+                      </button>
+                        <button onClick={descargarCSVU}className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                        <i className="fas fa-download fa-sm text-white-50"></i> Generate CSV Usuarios
                       </button>
                     </div>           
             
